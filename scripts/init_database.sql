425c68b0248f9825d022619a28be7f21997c05cc
nalinsaxena22 <nalin.saxena0@gmail.com>
2026-01-04 22:36:03 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,42 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DairyGoods' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DairyGoods' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
 
+USE MASTER;
+GO
+
+-- Drop and recreate the 'DairyGoods' database.
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DairyGoods')
+BEGIN
+	ALTER DATABASE DairyGoods SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DairyGoods;
+END;
+GO
+
+-- Create database 'DairyGoods'.
+CREATE DATABASE DairyGoods;
+GO
+
+USE DairyGoods;
+GO
+
+-- Create schemas.
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
