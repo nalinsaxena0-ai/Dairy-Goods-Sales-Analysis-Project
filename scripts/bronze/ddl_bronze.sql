8bb7ae0a65fc22dcb5a12a0fd01752fa5e18321e
nalinsaxena22 <nalin.saxena0@gmail.com>
2026-01-06 01:14:27 +0530

    Create bronze schema tables for land and product info
    This script creates the 'land_info' and 'prd_info' tables in the 'bronze' schema, dropping them if they already exist.

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,48 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+IF OBJECT_ID('bronze.land_info', 'U') IS NOT NULL
+	DROP TABLE bronze.land_info;
+GO
+
+CREATE TABLE bronze.land_info (
+Location			NVARCHAR(50),
+total_land_area		INT,
+number_of_cows		INT,
+farm_size			NVARCHAR(50),
+date				DATE
+);
+GO
+
+IF OBJECT_ID('bronze.prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.prd_info;
+
+CREATE TABLE bronze.prd_info (
+product_id			INT,
+product_name		NVARCHAR(50),
+brand				NVARCHAR(50),
+quantity			INT,
+price_per_unit		INT,
+total_value			INT,
+shelf_life			INT,
+storage_condition	NVARCHAR(50),
+production_date		DATE,
+Expiration_date		DATE,
+quantity_sold		INT,
+selling_price		INT,
+revenue				INT,
+customer_location	NVARCHAR(50),
+sales_channel		NVARCHAR(50),
+stk_quantity		INT,
+min_stk_threshold	INT,
+reorder_quantity	INT
+);
+GO
