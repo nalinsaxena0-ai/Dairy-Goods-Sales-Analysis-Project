2b8b8b3a3cc6c03ab0a901df293ed24a9f7e858a
nalinsaxena22 <nalin.saxena0@gmail.com>
2026-01-07 06:27:09 +0530

    Create ddl_silver.sql

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,90 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+IF OBJECT_ID ('silver.farms', 'U') IS NOT NULL
+    DROP TABLE silver.farms;
+GO
+
+CREATE TABLE silver.farms (
+    farm_id             INT IDENTITY(1,1) PRIMARY KEY,
+    location            NVARCHAR(100),
+    total_land_area     FLOAT,             -- value only
+    land_area_unit      NVARCHAR(20),      -- e.g., 'acres'
+    number_of_cows      INT,
+    farm_size           NVARCHAR(50),
+    record_date         DATE
+);
+GO
+
+IF OBJECT_ID ('silver.products', 'U') IS NOT NULL
+    DROP TABLE silver.products;
+GO
+
+CREATE TABLE silver.products (
+    product_id          INT PRIMARY KEY,
+    product_name        NVARCHAR(100),
+    brand               NVARCHAR(100),
+    shelf_life          INT,                -- days
+    storage_condition   NVARCHAR(100),
+    production_date     DATE,
+    expiration_date     DATE
+);
+GO
+
+IF OBJECT_ID ('silver.sales', 'U') IS NOT NULL
+    DROP TABLE silver.sales;
+GO
+
+CREATE TABLE silver.sales (
+    sale_id                 INT IDENTITY(1,1) PRIMARY KEY,
+    product_id              INT,
+
+    quantity_value          FLOAT,
+    quantity_unit           NVARCHAR(20),
+
+    price_per_unit_value    FLOAT,
+    price_per_unit_unit     NVARCHAR(20),
+
+    quantity_sold_value     FLOAT,
+    quantity_sold_unit      NVARCHAR(20),
+
+    selling_price_value     FLOAT,
+    selling_price_unit      NVARCHAR(20),
+
+    revenue                 FLOAT,     -- always INR
+    total_value             FLOAT,     -- always INR
+
+    customer_location       NVARCHAR(100),
+    sales_channel           NVARCHAR(50),
+
+    FOREIGN KEY (product_id) REFERENCES silver.products(product_id)
+);
+GO
+
+IF OBJECT_ID ('silver.inventory', 'U') IS NOT NULL
+    DROP TABLE silver.inventory;
+GO
+
+CREATE TABLE silver.inventory (
+    product_id              INT PRIMARY KEY,
+
+    stock_value             FLOAT,
+    stock_unit              NVARCHAR(20),
+
+    min_stock_value         FLOAT,
+    min_stock_unit          NVARCHAR(20),
+
+    reorder_quantity_value  FLOAT,
+    reorder_quantity_unit   NVARCHAR(20),
+
+    FOREIGN KEY (product_id) REFERENCES silver.products(product_id)
+);
+GO
